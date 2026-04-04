12401d5b5895e747666ff56b88de0fa2df2061f3
franknesling <matthias.gnilsen@hotmail.com>
2026-04-04 04:10:33 +0200

    total score small adjustment

--- EasterEpp/GameState.cs
+++ EasterEpp/GameState.cs
@@ -5,7 +5,7 @@ namespace EasterEpp
   public class GameState
   {
     public int TotalFound { get; set; }
-    public int TotalAvailable { get; set; } = 50;
+    public int TotalAvailable { get; set; } = 9 * 3;
 
     public Dictionary<string, int> FoundPerImage { get; set; } = new();
   }
